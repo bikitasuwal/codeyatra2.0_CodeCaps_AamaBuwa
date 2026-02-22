4a75d12c4ab9aebe69684cf67d2872d4d350c578
sam <awalsam2004@gmail.com>
2026-02-22 20:32:00 +0545

    2. Add task page and update home structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './Pages/Child/home'
+import Task from './Pages/Child/Task'
 
 function App() {
 
   return (
     <>
       <Home />
+      <Task />
     </>
   )
 }
