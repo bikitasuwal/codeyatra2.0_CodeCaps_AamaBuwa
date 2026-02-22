02dd89e38b84dcd989439dd935a0d3d4c791113b
Bikita Suwal <139045699+bikitasuwal@users.noreply.github.com>
2026-02-22 21:10:09 +0545

    added dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Child/Home"
 import Task from "./Pages/Child/Task"
+import Dashboard from "./Pages/Dashboard";
 
 function App() {
   return (
 
     <Routes>
-      <Route path="/" element={ <Home/> } />
+      <Route path="/" element={ <Dashboard/> } />
       <Route path="/task" element={ <Task/> } />
+      <Route path="/home" element={ <Home/> } />
     </Routes>
   );
 
